31ab5b5e2b0b03ec71fcc55b05851be3c297590c
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2022-08-30 18:45:05 +0300

    add logo,favicon,  login and reg

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import {Auth} from "./Auth/Auth";
+import { Route, Routes } from 'react-router-dom'
+import { Auth } from './Auth/Auth'
 
 export const App = () => {
-    return (
-        <div>
-            <Routes>
-                <Route path='auth' element={<Auth/>}>
-                    <Route index element={<h2>login</h2>}/>
-                    <Route path='registration' element={<h2>reg</h2>}/>
-                </Route>
-            </Routes>
-        </div>
-    )
+  return (
+    <div>
+      <Routes>
+        <Route path='auth' element={<Auth />}>
+          <Route index element={<h2>login</h2>} />
+          <Route path='registration' element={<h2>reg</h2>} />
+        </Route>
+      </Routes>
+    </div>
+  )
 }
--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import {Outlet} from "react-router-dom";
+import { Outlet } from 'react-router-dom'
+import logo from '../../Assets/Images/logo.svg'
 import './auth.scss'
 
 export const Auth = () => {
-    return <div className='auth'>
-        <svg>
-            <img src="" alt=""/>
-        </svg>
+  return (
+    <div className='auth'>
+      <div className='auth_logo'>
+        <img src={logo} alt='logo' />
         <div>
-            <Outlet/>
-            <div><h1>TEST!</h1></div>
+          <h2>YourVocabulary</h2>
+          <h3>App</h3>
         </div>
+      </div>
+      <div>
+        <Outlet />
+        <div>
+          <h1>TEST!</h1>
+        </div>
+      </div>
     </div>
+  )
 }

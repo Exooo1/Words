9781f16db449ff25d4359047c3901ae30b3f948f
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2022-11-04 15:53:29 +0300

    fixed bugs and addd new feature

--- src/Redux/Reselect.ts
+++ src/Redux/Reselect.ts
@@ -1,3 +1,4 @@
 import { AppRootState } from './ReduxUtils'
 
 export const authReselect = (state: AppRootState) => state.authReducer.auth
+export const profileReselect = (state: AppRootState) => state.wordsSlice
--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,15 +1,19 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import thunkMiddleware from 'redux-thunk'
-import { errorsReducer } from './ErrorsReducer'
-import { authReducer } from './AuthReducer'
-import { wordsSlice } from './WordsReducer'
+import {errorsReducer} from './ErrorsReducer'
+import {authReducer} from './AuthReducer'
+import {wordsSlice} from './WordsReducer'
+import {profileReducer} from "./ProfileReducer";
+import {wordApi} from "./WordRTK";
 
 const reducers = combineReducers({
-  errorsReducer,
-  authReducer,
-  wordsSlice,
+    errorsReducer,
+    authReducer,
+    wordsSlice,
+    profileReducer,
+    [wordApi.reducerPath]: wordApi.reducer
 })
 export const store = configureStore({
-  reducer: reducers,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware),
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(wordApi.middleware,thunkMiddleware),
 })

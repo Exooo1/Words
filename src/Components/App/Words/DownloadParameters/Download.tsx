f26cd5189de27bbdd5e94809effddf1f0ea9b3cd
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-01-08 21:54:51 +0300

    added new logic for Pagination

---
+++ src/Components/App/Words/DownloadParameters/Download.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type DownloadType = {
+    handlerFile: (value: string) => void
+    file: string
+    downloadFile: () => void
+}
+
+export const Download: React.FC<DownloadType> = React.memo(({downloadFile, file, handlerFile}) => {
+    return <div>
+        <p onClick={downloadFile}>Download File</p>
+        <select value={file} onChange={(e) => handlerFile(e.target.value)}>
+            <option value='txt'>.txt</option>
+            <option value='pdf'>.pdf</option>
+            <option value='doc'>.doc</option>
+        </select>
+    </div>
+})

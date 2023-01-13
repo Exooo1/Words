bb4c54a95ea025b65ac0453478eaceefe88a4b17
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-01-13 21:46:10 +0300

    correct usefulFunc

--- src/Common/usefulFuncs.ts
+++ src/Common/usefulFuncs.ts
@@ -1,19 +1,20 @@
-import { addHint, deleteHint, PayloadHintType } from '../Redux/ErrorsReducer'
+import {addHint, deleteHint, PayloadHintType} from '../Redux/ErrorsReducer'
 
 type StatusTypeHint = 'done' | 'error'
 
 export const changeTitle = (text: string) => {
-  const title = document.getElementsByTagName('title')
-  title[0].innerText = text
+    const title = document.getElementsByTagName('title')
+    title[0].innerText = text
 }
 export const handlerDeleteHint = (
-  value: string,
-  dispatch: (item: { payload: PayloadHintType | string }) => void,
-  status: StatusTypeHint,
+    value: string,
+    dispatch: (item: { payload: PayloadHintType | string }) => void,
+    status: StatusTypeHint,
 ) => {
-  const v4id = (Math.random() * 10).toString(36).slice(2)
-  dispatch(addHint({ v4id, article: value, status }))
-  setTimeout(() => {
-    dispatch(deleteHint(v4id))
-  }, 5000)
+    const v4id = (Math.random() * 10).toString(36).slice(2)
+    console.log(addHint({v4id, article: value, status}))
+    dispatch(addHint({v4id, article: value, status}))
+    setTimeout(() => {
+        dispatch(deleteHint(v4id))
+    }, 5000)
 }

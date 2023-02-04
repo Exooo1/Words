32e4aef5191d375ba32fba385d9112c53521773f
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-02-04 21:51:18 +0300

    change design in auth, and added new file

--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -1,30 +1,21 @@
-import React, { useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
-import { Slider } from '../Slider/Slider'
-import { HintModal } from '../../Common/ModalComponents/HintModal/HintModal'
-import { changeTitle } from '../../Common/usefulFuncs'
-import logo from '../../Assets/Images/logo.png'
+import React, {useEffect} from 'react'
+import {Outlet} from 'react-router-dom'
+import {HintModal} from '../../Common/ModalComponents/HintModal/HintModal'
+import {changeTitle} from '../../Common/usefulFuncs'
+
 import styles from './auth.module.scss'
 
 export const Auth = () => {
-  useEffect(() => {
-    changeTitle('Auth')
-  }, [])
+    useEffect(() => {
+        changeTitle('Auth')
+    }, [])
 
-  return (
-    <div className={styles.auth}>
-      <div className={styles.auth_logo}>
-        <img src={logo} alt='logo' />
-        <div>
-          <h2>YourVocabulary</h2>
-          <h3>App</h3>
-        </div>
-      </div>
-      <div className={styles.auth_outlet}>
-        <Outlet />
-        <Slider />
-      </div>
-      <HintModal />
-    </div>
-  )
+    return (
+        <section className={styles.auth}>
+            <section className={styles.auth_outlet}>
+                <Outlet/>
+            </section>
+            <HintModal/>
+        </section>
+    )
 }
---
+++ src/Components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return <footer>
+        <cite>Vlas Maskalenchik</cite>
+        <time>@2022-2023</time>
+        <address>Minsk</address>
+    </footer>
+}
